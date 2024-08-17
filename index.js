9db32dfc0c83d205d04cdc1e228372d4286355ef
nhnaahid <nhnahid.iu@gmail.com>
2024-08-17 17:16:27 +0600

    after deploy to vercel-seccess

--- index.js
+++ index.js
@@ -7,7 +7,14 @@ const port = process.env.PORT || 5000;
 
 // middleware
 app.use(express.json());
-app.use(cors());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+        "http://localhost:5174",
+        "https://fabrico-d5192.web.app",
+        "https://fabrico-d5192.firebaseapp.com"
+    ]
+}));
 
 
 
@@ -64,7 +71,7 @@ async function run() {
 
 
         // await client.db("admin").command({ ping: 1 });
-        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+        // console.log("Pinged your deployment. You successfully connected to MongoDB!");
     } finally {
         // Ensures that the client will close when you finish/error
         // await client.close();
